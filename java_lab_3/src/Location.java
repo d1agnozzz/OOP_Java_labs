50cd9153ee1cccf451b4b791ad2a09ebeda45eb8
Alex Mit <klepoz.672@mail.ru>
2021-04-06 03:52:50 +0300

    commented for Wi1d10neW01f

--- java_lab_3/src/Location.java
+++ java_lab_3/src/Location.java
@@ -24,22 +24,25 @@ public class Location
         this(0, 0);
     }
 
-//    public boolean equals(Location location){
-//        return (this.xCoord == location.xCoord && this.yCoord == location.yCoord);
-//    }
+    // переопределяем методы equals и hashCode для корректной работы нашего класса в составе HashMap
 
-    @Override
+    @Override // переопределение метода базового класса
     public boolean equals(Object obj){
-        if (!(obj instanceof Location)) return false;
-        if (this == obj) return true;
+        if (!(obj instanceof Location)) return false; // если объект другого класса
+        if (this == obj) return true; // если ссылки равны
 
+        // приводим объект к классу Location
         Location location = (Location)obj;
+
+        // сравниваем значения полей
         return (this.xCoord == location.xCoord &&
                 this.yCoord == location.yCoord);
     }
 
-    @Override
+    @Override // переопределение метода базового класса
     public int hashCode(){
+        // домножаем x на простое число, чтобы различать (a, b) и (b, a)
+        // берется простое число для уменьшения количества коллизий
         return xCoord * 31 + yCoord;
     }
 }
